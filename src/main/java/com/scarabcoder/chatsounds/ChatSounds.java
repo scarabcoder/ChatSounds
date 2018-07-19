9b439d57dbba678af89fc7f9c8ab17cdb040f060
Nicholas Harris (ScarabCoder) <scarabcoder@gmail.com>
2018-07-19 14:00:42 -0700

    Initial Commit

---
+++ src/main/java/com/scarabcoder/chatsounds/ChatSounds.java
@@ -0,0 +1,16 @@
+package com.scarabcoder.chatsounds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatSounds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+
+    }
+
+
+}
